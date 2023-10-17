314d9ab2e244ee98c64ff5c1b127e4f7b0009cf6
Tako nak <120483541+takonak@users.noreply.github.com>
2023-10-18 00:06:03 +0400

    Add files via upload

---
+++ lesson1.js
@@ -0,0 +1,27 @@
+
+// Task 1
+
+var userName= prompt("Please enter your name");
+var userSurname = prompt ("Please enter your surname");
+var userAge = prompt("Please enter your age");
+
+console.log(userName);
+console.log(userSurname);
+console.log(userAge);
+
+// Task 2
+
+var clientName = "Tako";
+var clientSurname = "Nakashidze";
+var clientAge = "27";
+var clientEmail = "t.nakashidze.1@gmail.com";
+var clientGender = "Female";
+
+
+var fullText = `<h1>Hello, my name is ${clientName}, I am ${clientAge} years old. My e-mail address is ${clientEmail}, I am ${clientGender}.</h1>`;
+document.write(fullText);
+
+// task 3
+
+var fullName = prompt("Please enter your fullname");
+document.write("<H1>"+fullName+"</h1>");
